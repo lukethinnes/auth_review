177164c55ce8d327548e4660e2fe7d57e90913d7
Luke Thinnes <lukethinnes@yahoo.com>
2021-10-06 13:24:27 -0600

    Added JS functionality and enabled CORS.

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,24 @@
-console.log('yep')
+const loginForm = document.querySelector('.login')
+
+loginForm.addEventListener("submit", handleLogin)
+
+function handleLogin(event) {
+  event.preventDefault()
+
+  const loginFormData = new FormData(event.target)
+  const username = loginFormData.get("username")
+  const password = loginFormData.get("password")
+
+  const loginBody = { user: { username, password } }
+
+  fetch("http://localhost:3000/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: {
+      
+    }
+  }
+  event.target.reset()
+}
